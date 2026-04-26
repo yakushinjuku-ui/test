2925724b887524aaeb4b7f76a3c75d1b00cb8894
yakushinjuku-ui <yakushinjuku@gmail.com>
2026-04-26 21:28:38 +0900

    Add files via upload

---
+++ words.js
@@ -0,0 +1,7 @@
+const words = [
+{word:"abundant", meaning:"豊富な"},
+{word:"ancient", meaning:"古代の"},
+{word:"brave", meaning:"勇敢な"},
+{word:"calm", meaning:"冷静な"},
+{word:"delicious", meaning:"おいしい"}
+];
